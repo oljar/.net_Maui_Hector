acfed470e27d297411ed2e707ca6420630e0a55c
oljar <oljar@tlen.pl>
2026-02-15 05:30:31 +0100

    FlexLayout

---
+++ FlexLayout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlexLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new FlexLayout();
+        }
+    }
+}
---
+++ FlexLayout/FlexLayout.xaml.cs
@@ -0,0 +1,9 @@
+namespace FlexLayout;
+
+public partial class FlexLayout : ContentPage
+{
+	public FlexLayout()
+	{
+		InitializeComponent();
+	}
+}
